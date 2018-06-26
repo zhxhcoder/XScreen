7466865429e8ccea9fa5186ebb78f64a32864467
zhxh <zhxhcoder@gmail.com>
2018-06-26 16:58:11 +0800

    XText

--- xtextlib/src/main/java/com/zhxh/android/xtextlib/XText.java
+++ xtextlib/src/main/java/com/zhxh/android/xtextlib/XText.java
@@ -1,10 +1,16 @@
 package com.zhxh.android.xtextlib;
 
+import android.content.Context;
 import android.graphics.Paint;
 import android.text.SpannableString;
 import android.text.Spanned;
+import android.text.TextUtils;
 import android.text.style.AbsoluteSizeSpan;
 import android.text.style.ForegroundColorSpan;
+import android.util.TypedValue;
+import android.view.Gravity;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,6 +22,7 @@ public class XText {
 
     /**
      * 获取字体宽度
+     *
      * @param str      字体
      * @param fontSize 字体sp
      * @return 像素
@@ -38,6 +45,7 @@ public class XText {
         }
         return sp;
     }
+
     public static SpannableString getSpanSizeReg(String srcStr, String regularExpression, int size) {
         SpannableString resultSpan = new SpannableString(srcStr);
         Pattern p = Pattern.compile(regularExpression);
@@ -57,5 +65,47 @@ public class XText {
             stringList.add(m.group());
 
         return stringList;
+    }
+
+    public static void showTagsView(Context ctx, List<String> tags, LinearLayout ll_tags_container, int colorType) {
+
+        if (null != tags && !tags.isEmpty()) {
+
+            ll_tags_container.removeAllViews();
+
+            for (int j = 0; j < tags.size(); j++) {
+
+                TextView tagText = new TextView(ctx);
+                LinearLayout.LayoutParams paramsLayout = new LinearLayout.
+                        LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, 30);
+
+                paramsLayout.setMargins(5, 10, 5, 10);
+                tagText.setPadding(8, 0, 8, 0);
+                tagText.setSingleLine(true);
+                tagText.setEllipsize(TextUtils.TruncateAt.END);
+                tagText.setGravity(Gravity.CENTER);
+                switch (colorType) {
+                    case 0:
+                        //tagText.setTextColor(ctx.getResources().getColor(R.color.fund_operate_red));
+                        //tagText.setBackgroundResource(R.drawable.shape_button_white_redoutside);
+                        break;
+                    case 1:
+                        //tagText.setTextColor(ctx.getResources().getColor(R.color.fund_operate_blue));
+                        //tagText.setBackgroundResource(R.drawable.shape_button_white_blueoutside_transparentinside);
+                        break;
+                    default:
+                        //tagText.setTextColor(ctx.getResources().getColor(R.color.fund_operate_red));
+                        //tagText.setBackgroundResource(R.drawable.shape_button_white_redoutside);
+                        break;
+                }
+
+                tagText.setTextSize(TypedValue.COMPLEX_UNIT_SP, 10);
+                tagText.setLayoutParams(paramsLayout);
+                tagText.setText(tags.get(j));
+                ll_tags_container.addView(tagText);
+
+            }
+
+        }
     }
 }
