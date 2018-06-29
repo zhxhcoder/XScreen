3517a0673cc324628084065a4029adac602febe5
zhxh <zhxhcoder@gmail.com>
2018-06-29 11:58:29 +0800

    config version 1.5

--- app/src/main/java/com/zhxh/xtext/MainActivity.java
+++ app/src/main/java/com/zhxh/xtext/MainActivity.java
@@ -1,7 +1,9 @@
 package com.zhxh.xtext;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView tv1 = findViewById(R.id.tv1);
+        TextView tv2 = findViewById(R.id.tv2);
+        TextView tv3 = findViewById(R.id.tv3);
+
+        tv1.setTextColor(Color.parseColor("#0000CD"));
+        tv2.setTextColor(Color.parseColor("#ff0000CD"));
+        tv3.setTextColor(Color.parseColor("#FFD700"));
     }
 }
